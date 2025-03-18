a45b2308145a6364346e9a10f2889b13ab6f9fa3
dzarrr <dzarbelahanifa@gmail.com>
2025-03-18 19:54:14 +0700

    fix: use val from useRequest

--- src/pages/List/ListPage.tsx
+++ src/pages/List/ListPage.tsx
@@ -1,4 +1,5 @@
 import { useRequest } from "ahooks";
+import { useEffect } from "react";
 import { getPopularMovies } from "../../services/movieService";
 
 export default function ListPage() {
@@ -9,5 +10,14 @@ export default function ListPage() {
     onSuccess: (data) => console.log(data),
   });
 
-  return <div>this is the list fpage</div>;
+  useEffect(() => {
+    console.log(movieData);
+  }, [movieData]);
+
+  return (
+    <div>
+      {"this is the list fpage"}
+      <span>{loading && "loading..."}</span>
+    </div>
+  );
 }
